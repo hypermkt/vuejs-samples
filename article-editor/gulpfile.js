e383614f9795d6ba7db7c4b7b5195d29ba396f36
Makoto Chiba <hypermkt@gmail.com>
2016-07-11 16:02:21 +0900

    vueifyを使う

--- article-editor/gulpfile.js
+++ article-editor/gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var vueify = require('vueify');
 var source= require('vinyl-source-stream');
 
 // browserifyを実行しファイル結合を行う
 gulp.task('browserify', function() {
     browserify({
-          entries: ['./main.js']
-        })
-      .bundle()
+        entries: ['./main.js'],
+        transform: ['vueify']
+      })
+    .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./main.js'], ['browserify']);
+  gulp.watch(['./main.js', './components/*.vue'], ['browserify']);
 });
